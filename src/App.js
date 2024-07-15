70e305ccb9b931902935f1b654f68e88a5a91d69
game1095 <taweesak.in@gmail.com>
2024-07-15 19:55:42 +0700

    second day

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import AddForm from "./components/AddForm";
+import Item from "./components/Item";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "งานแรกของฉัน" },
+    { id: 2, title: "คู่มือการใช้โปรแกรม" },
+    { id: 3, title: "ไปเที่ยว" },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [editId, setEditId] = useState(null);
+
+  function saveTask(e) {
+    e.preventDefault();
+    if (!title) {
+      alert("กรุณาป้อนข้อมูล");
+    } else if (editId) {
+      // อัพเดทข้อมูล
+      const updateTask = tasks.map((item) => {
+        // รายการใดมีรหัสตรงกับรหัสแก้ไข
+        if (item.id === editId) {
+          return { ...item, title: title };
+        }
+        return item;
+      });
+      setTasks(updateTask);
+      setEditId(null);
+      setTitle("");
+    } else {
+      // เพิ่มข้อมูล
+      const newTask = {
+        id: Math.floor(Math.random() * 1000),
+        title: title,
+      };
+      setTasks([...tasks, newTask]);
+      setTitle("");
+    }
+  }
+
+  function editTask(id) {
+    setEditId(id);
+    const editTask = tasks.find((item) => item.id === id);
+    setTitle(editTask.title);
+  }
+
+  function deleteTask(id) {
+    const result = tasks.filter((item) => item.id !== id);
+    setTasks(result);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container">
+        <AddForm
+          title={title}
+          setTitle={setTitle}
+          saveTask={saveTask}
+          editId={editId}
+        />
+        <section>
+          {tasks.map((data) => (
+            <Item
+              key={data.id}
+              data={data}
+              deleteTask={deleteTask}
+              editTask={editTask}
+            />
+          ))}
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import "./Item.css";
+export default function Item(props) {
+  const { data, deleteTask , editTask } = props;
+  return (
+    <div className="list-item">
+      <p className="title">{data.title}</p>
+      <div className="button-container">
+        <button className="btn" onClick={() => deleteTask(data.id)}>
+          ลบ
+        </button>
+        <button className="btn" onClick={() => editTask(data.id)}>แก้ไข</button>
+      </div>
+    </div>
+  );
+}
